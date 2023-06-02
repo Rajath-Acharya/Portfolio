fa24752c079ce8f8f8ebcd8d800a8a232f3eb1b7
Rajath-Acharya <rajath365@gmail.com>
2023-06-02 23:10:00 +0530

    adding social media icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import github from '../../public/github.svg';
+import linkedIn from '../../public/linkedin.svg';
+
 export default function Home() {
   return (
     <section>
@@ -7,6 +11,22 @@ export default function Home() {
       <p>
         A Software Developer based in Mangalore, Karnataka, India.
       </p>
+      <article className='flex'> 
+        <a href='https://github.com/Rajath-Acharya' target="_blank">
+          <Image
+            priority
+            src={github}
+            alt="github"
+          />
+        </a>
+        <a href='https://www.linkedin.com/in/rajath-acharya-068194135/' target="_blank">
+          <Image
+            priority
+            src={linkedIn}
+            alt="linkedIn"
+          />
+        </a>
+      </article>
     </section>
   )
 }
