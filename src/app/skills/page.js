7a8606de4c71bd55f4f2591f8392c5cea902db64
Rajath-Acharya <rajath365@gmail.com>
2023-06-08 11:34:57 +0530

    setting up skills route

---
+++ src/app/skills/page.js
@@ -0,0 +1,12 @@
+export default function Experience() {
+  return (
+    <div>
+      <section>
+        <h1>Technical Skills</h1>
+      </section>
+      <section>
+        <h1>Soft Skills</h1>
+      </section>
+    </div>
+  )
+}
